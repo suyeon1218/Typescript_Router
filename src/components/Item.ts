b257db3941cf69259f241716e91f9ad070f00409
Suyeon <msjgr2801@gmail.com>
2024-05-23 18:40:20 +0900

    feat: item 컴포넌트 템플릿 작성

--- src/components/Item.ts
+++ src/components/Item.ts
@@ -1,5 +1,11 @@
 import Component from '~/core/components/Component';
 
-class Item extends Component {}
+class Item extends Component {
+  template(): string {
+    return `
+        <div>item</div>
+      `;
+  }
+}
 
 export default Item;
