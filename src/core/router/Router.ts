ff944a56481d11ee6a6a207ffeacc9253f30f281
Suyeon <msjgr2801@gmail.com>
2024-05-23 18:40:35 +0900

    feat: children 페이지 렌더링하기

--- src/core/router/Router.ts
+++ src/core/router/Router.ts
@@ -15,10 +15,14 @@ export type CurrRoute = Route & { params: Params | undefined };
 
 class Router {
   private routes: Route[] | undefined;
+  outlets: CurrRoute[];
   currRoutes: CurrRoute[];
+  observes: (() => void)[];
 
   constructor() {
+    this.outlets = [];
     this.currRoutes = [];
+    this.observes = [];
   }
 
   createRouter(routes: Route[]) {
@@ -35,7 +39,25 @@ class Router {
 
   routing() {
     if (this.routes) {
-      this.currRoutes = findRoute(location.pathname, '', this.routes);
+      const nextRoutes = findRoute(location.pathname, '/', this.routes);
+      this.outlets = [...nextRoutes];
+
+      if (this.observes.length > 0) {
+        let lastRender = this.observes[this.observes.length - 1];
+
+        for (let index = 0; index < this.currRoutes.length - 1; index++) {
+          if (this.currRoutes[index].path !== nextRoutes[index].path) {
+            lastRender = this.observes[index];
+            break;
+          } else {
+            this.outlets.pop();
+          }
+        }
+        lastRender && lastRender();
+      } else {
+        this.outlets = [...nextRoutes];
+      }
+      this.currRoutes = nextRoutes;
     }
   }
 }
--- src/core/router/Service.ts
+++ src/core/router/Service.ts
@@ -1,9 +1,18 @@
-import Router, { RouteType } from './Router';
+import Component from '../components/Component';
+import Router, { Route } from './Router';
 
-export function createRouter(routes: RouteType[]) {
+export function createRouter(routes: Route[]) {
   return Router.createRouter(routes);
 }
 
 export function navigate(url: string) {
   return Router.navigate(url);
+}
+
+export function Outlet(observe: () => void) {
+  const $element = Router.outlets.pop()?.element;
+
+  Router.observes.push(observe);
+
+  return $element as typeof Component;
 }
