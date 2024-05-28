17574f48f6646d957272cb2a1af90921f85c4950
Suyeon <msjgr2801@gmail.com>
2024-05-28 18:33:25 +0900

    feat: 파라미터 가져와서 item 에 출력하기

--- src/components/Item.ts
+++ src/components/Item.ts
@@ -1,9 +1,21 @@
-import Component from '~/core/components/Component';
+import Component, { ComponentProps } from '~/core/components/Component';
+import { Params, getParams } from '~/core/router';
 
 class Item extends Component {
+  params: Params | undefined;
+
+  constructor({ $target }: ComponentProps) {
+    super({ $target });
+    this.params = undefined;
+  }
+
+  beforeMount(): void {
+    this.params = getParams();
+  }
+
   template(): string {
     return `
-        <div>item</div>
+        <div>item${this.params ? this.params.id : ''}</div>
       `;
   }
 }
